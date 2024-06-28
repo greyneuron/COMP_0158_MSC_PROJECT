913304b517fa15c00ed2fa97eb337f835994482d
greyneuron <pllowry@hotmail.com>
2024-06-28 16:22:01 +0100

    Read fasta into DB works

--- protein_db.py
+++ protein_db.py
@@ -1,25 +1,33 @@
 import duckdb
 
 class ProteinDB:
+    db_string = "/Users/patrick/dev/ucl/comp0158_mscproject/database/proteins.db"
+    
     def __init__(self):
         print('ProteinDB')
     
     
     def create_tables(self):
         # Connect in memory
-        con = duckdb.connect(database=':memory:')
-
+        
+        #con = duckdb.connect(database=':memory:')
+        con = duckdb.connect(database=ProteinDB.db_string)
+        
         # Create table
-        duckdb.sql("\
+        con.execute("\
             CREATE TABLE PROTEIN_SENTENCE (\
                 UNIPROT_ID VARCHAR,\
+                IPR VARCHAR,\
                 DESCRIPTION VARCHAR,\
-                NAME VARCHAR,\
+                TAX_NAME VARCHAR,\
                 TAX_ID VARCHAR,\
-                TAX_DESCRIPTION VARCHAR\
+                DOM_TYPE VARCHAR,\
+                START_POS USMALLINT,\
+                END_POS USMALLINT\
             )")
 
-        duckdb.sql("\
+        #Q6GZX4	IPRXXXXXX	Putative transcription factor 001R n=4 Tax=Ranavirus TaxID=10492 RepID=001R_FRG3G	LowComplexity	1	257
+        con.execute("\
             CREATE TABLE PROTEIN_WORD (\
                 UNIPROT_ID VARCHAR,\
                 WORD_TYPE VARCHAR,\
@@ -32,9 +40,17 @@ class ProteinDB:
         # Close the connection
         con.close()
         
-    def describe_tables(self):
-        result = duckdb.sql('DESCRIBE PROTEIN_SENTENCE')
-        print(result)
-        result = duckdb.sql('DESCRIBE PROTEIN_WORD')
-        print(result)
+    def create_indexes(self):
+        con = duckdb.connect(database=ProteinDB.db_string)
+        con.execute("CREATE INDEX w_protid_idx ON PROTEIN_WORD(UNIPROT_ID)")
+        con.execute("CREATE INDEX w_refid_idx ON PROTEIN_WORD(REF_ID)")
+        con.execute("CREATE INDEX p_protid_idx ON PROTEIN_SENTENCE(UNIPROT_ID)")
+        con.close()
         
+    def describe_tables(self):
+        con = duckdb.connect(database=ProteinDB.db_string)
+        result = con.execute("DESCRIBE PROTEIN_SENTENCE")
+        print(result)
+        result = con.execute("DESCRIBE PROTEIN_WORD")
+        print(result)
+        con.close()
