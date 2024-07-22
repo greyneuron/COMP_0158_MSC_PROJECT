c7f9bd26b3fb9e0162558862e64a3be84d6387a9
greyneuron <pllowry@hotmail.com>
2024-07-22 17:48:24 +0100

    Monday 22 Jul close of play

--- code/mysql_tools.py
+++ code/mysql_tools.py
@@ -2,7 +2,9 @@ import mysql.connector
 import time
 
 
-
+#
+# Used on 22 July to create pre-cprpus
+#
 def create_pre_corpus(from_record, chunk_size, iteration):
     
     output_name     = "precorpus/pre_corpus_20240722_" + str(iteration) + '.dat'
@@ -53,50 +55,42 @@ def create_pre_corpus(from_record, chunk_size, iteration):
         con.close()
         return 
         
-    
 
-#
-# This took 6.47 seconds to query 1M records from W2V_TOKEN
-#
-def output_query():
-    print('output query - trying to connect...')
+# queries unique pfam records
+def get_unique_pfam():
+    
+    output_name     = "unique_corpus_pfam_20240722.dat"
+    of  = open(output_name, "w")
     
     try:
-        s = time.time()
         con = mysql.connector.connect(user='admin', password='w0rd2v3c', host='w2v-rds-db.cligs4ak0dtg.eu-west-1.rds.amazonaws.com', database='W2V')
-        #print('connected')
-        
+        #print('...connected')
         cursor = con.cursor()
         
-        #query = ("SELECT * FROM W2V_TOKEN WHERE UNIPROT_ID = 'A0A010PZU8'")
-        #query = "SELECT * FROM W2V_TOKEN ORDER BY UNIPROT_ID LIMIT 1000000" # query first 1M items
-        #query = "SELECT * FROM W2V_TOKEN ORDER BY UNIPROT_ID LIMIT 5000000, 10" # query first 10 items after 5M items - taking ages
-        
-        # these work
-        # select first n proteins and query all tokens - 100k took 4.29s, 1M took 23s
-        #query = "SELECT W2V_PROTEIN.UNIPROT_ID, W2V_TOKEN.* FROM (SELECT UNIPROT_ID FROM W2V_PROTEIN ORDER BY UNIPROT_ID LIMIT 1000000) AS W2V_PROTEIN INNER JOIN W2V_TOKEN AS W2V_TOKEN ON W2V_PROTEIN.UNIPROT_ID = W2V_TOKEN.UNIPROT_ID"
-        
-        # Select 1M proteins after the first 2M. Took 32s compared to 23s for first 1M
-        # Select 1M proteins after the first 10M. Query took 5s, overall took 48s
-        query = f"SELECT W2V_PROTEIN.UNIPROT_ID, W2V_TOKEN.* FROM (SELECT UNIPROT_ID FROM W2V_PROTEIN ORDER BY UNIPROT_ID LIMIT 10000000, 1000000 AS W2V_PROTEIN INNER JOIN W2V_TOKEN AS W2V_TOKEN ON W2V_PROTEIN.UNIPROT_ID = W2V_TOKEN.UNIPROT_ID"
-        
-        query_2 = f"SELECT * FROM W2V_TOKEN WHERE UNIPROT_ID = '{uniprot_id}'"
-
-        print('executing query....')
+        query = "SELECT DISTINCT TOKEN FROM W2V_TOKEN WHERE TYPE='PFAM' ORDER BY TOKEN"
         cursor.execute(query)
-        e = time.time()
-        print('query took', str(e-s), 'ms')
-        
-        for res in cursor:
-            print(res[0], '|', res[2], '|', res[3], '|', res[4], '|', res[5])
-        
-        e = time.time()
-        print('entire execution took', str(e-s), 'ms end to end')
-    except:
-        print('error connecting')
+
+        results = cursor.fetchall()
+
+        if(len(results) >0):
+            print(len(results), 'results returned')
+            for res in results:
+                of.write(res[0] + '\n')
+        else:
+            print('No results returned')  
+        of.close()
+        con.close()
+        return
+    except Exception as e:
+        print('Error connecting', e)
+        of.close()
+        con.close()
+        return 
+
+
     
-   
-def connect():
+# this is just to test the conection
+def connection_test():
     """ Connect to MySQL database """
     print('trying to connect...')
     
@@ -128,16 +122,17 @@ def connect():
         # close connection
         cursor.close()
         con.close()
-        
     except:
         print('error connecting')
 
 
-
+#
+# main
+#
 if __name__ == '__main__':
-    #connect()
-    #output_query()
-    
+
+    '''
+    # create pre-corpus file in chunks of 500k proteins
     start       = 0
     chunk_size  = 500000
     iteration   = 1
@@ -147,6 +142,10 @@ if __name__ == '__main__':
     while(result != -1):
         result = create_pre_corpus(start, chunk_size, iteration)
         iteration +=1
-        start += chunk_size
-        
+        start += chunk_size 
     print('...end')
+    '''
+    
+    #get unique pfam entries
+    print('gettting unique pfam entries')
+    get_unique_pfam()
