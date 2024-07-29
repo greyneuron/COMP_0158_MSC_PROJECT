4564297de31373d5f2efbfb03e5b28730bb1e78c
greyneuron <pllowry@hotmail.com>
2024-07-29 12:48:49 +0100

    successful calculation of encoding and converion to numpy array

--- code/corpus/encode_pfam_w2v.py
+++ code/corpus/encode_pfam_w2v.py
@@ -2,6 +2,7 @@ import time
 from gensim.models import Word2Vec
 import os
 import glob
+import numpy as np
 
 def encode_pfam(model, pfam_id):
     return 
@@ -9,14 +10,17 @@ def encode_pfam(model, pfam_id):
 model_file = "/Users/patrick/dev/ucl/comp0158_mscproject/code/corpus/my_w2v_model.model"
 pfam_file = "/Users/patrick/dev/ucl/comp0158_mscproject/code/corpus/encoding/unique_pfam_20240722.dat"
 
-
 print('loading model:', model_file)
 model = Word2Vec.load(model_file)
 
 try:
-    print('PF00001', model.wv["PF00001"])
-    print('PF00002', model.wv["PF00002"])
-    print('Hello', model.wv["Hello"])
+    for pfam_id in ["PF00001", "PF00002"] :
+        encoding = model.wv[pfam_id]
+        #print(pfam_id, len(encoding),  '\n',encoding)
+        
+        np_vector = np.array(encoding)
+        print(pfam_id, 'numpy array:\n', np_vector)
+
 except Exception as e:
         print(f"An error occurred: {e}")
     
