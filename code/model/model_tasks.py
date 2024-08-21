b32df0c025f122c128d9d990b0c0fa27ea2cd3b0
greyneuron <pllowry@hotmail.com>
2024-08-21 16:33:39 +0100

    prep for cluster

--- code/model/model_tasks.py
+++ code/model/model_tasks.py
@@ -10,14 +10,14 @@ import glob
 from scipy.spatial.distance import pdist, squareform
 from scipy.stats import pearsonr
 
-class W2V_ModleTasks:
+class W2V_ModelTasks:
     
-    def __init__(self, model_dir, vocab_dir, corpus_file):
-        print(f"W2V_ModleTasks\nInitialised with model directory \n{model_dir}\nand vocab dir\n{vocab_dir}")
-        self.model_dir = model_dir
-        self.vocab_dir = vocab_dir
-        self.corpus_file = corpus_file
-        self.sentences = None
+    def __init__(self, model_type, model_dir, corpus_file):
+        print(f"W2V_ModleTasks initialised with \n - model type: {model_type}\n - corpus file: {corpus_file}\n - target directory for models: {model_dir}")
+        self.model_type     = model_type
+        self.model_dir      = model_dir
+        self.corpus_file    = corpus_file
+        self.sentences      = None
         
     #
     # parses a dicorpus file to build up sentences to create a model
@@ -46,11 +46,10 @@ class W2V_ModleTasks:
     
     #
     # Create W2V model
-    #
-    def create_w2v_model(self, vector_size, window_size, min_count):
+    #   
+    def create_w2v_model(self, model_type, vector_size, window_size, min_count):
         current_date    = datetime.now().strftime('%Y%m%d')
-        
-        model_base_name = "w2v_"+current_date + "_v"+str(vector_size)+"_w"+str(window_size)
+        model_base_name = "w2v_"+current_date + "_sg"+str(model_type) +"_v"+str(vector_size)+"_w"+str(window_size)
         model_name      = self.model_dir+model_base_name + ".model"
         
         s = time.time()
@@ -59,11 +58,10 @@ class W2V_ModleTasks:
             self.get_corpus_sentences()
         
         # create model from sentences       
-        w2v = Word2Vec(self.sentences, vector_size=vector_size, window=window_size, workers=4, epochs=10, min_count=min_count)
+        w2v = Word2Vec(self.sentences, sg=model_type, vector_size=vector_size, window=window_size, workers=4, epochs=10, min_count=min_count)
         
         # save model      
         w2v.save(model_name)
-        
         
         e = time.time()
         print(f"created {model_base_name}, v:{vector_size}, w:{window_size}, ms:{mc}, time:{round(e-s,2)}s, output:{model_name}")
